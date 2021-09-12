3deeb398852715b4131d2c2c64f2d437d42b8d7f
AJMIK <ajmimkabeer24@gmail.com>
2021-09-12 12:17:58 +0530

    code added

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var express=require('express')
+var bodyParser = require('body-parser')
+var app=express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.post('/',(req,res)=>{
+
+    var getName= req.body.name
+    var getRoll= req.body.rollno
+    res.json(getName + getRoll)
+})
+
+app.post('/read',(req,res)=>{
+
+    var getNum1= parseFloat(req.body.num1)
+    var getNum2=parseFloat(req.body.num2)
+    var sum= getNum1 + getNum2
+
+    res.json({"result":sum})
+})
+app.listen(3000,()=>{
+    console.json("server started at http://localhost:3000/")
+})
